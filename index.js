29b8b55bfd0e986a3369491965c1cd4c35bed2b6
森　泰樹 <taiki@mbp-2.local>
2019-05-02 15:52:51 +0900

    [update]動画ダウンロードのプルダウンその他にテキスエリアを追加しました。

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
---// import './css/reset.scss';
+// import './css/reset.scss';
 // import './css/main.scss';
 // import './index.html';
 // import flatpickr from "flatpickr";
@@ -212,4 +212,5 @@ document.addEventListener('DOMContentLoaded', function(){
             
         })    
     }
+
 });
